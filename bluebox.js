a571c8ce1d4e32ee9557c7e2ba87f3e7e1f53134
FilipMiscevic <filip.miscevic@gmail.com>
2019-04-06 04:27:23 -0400

    Added extra DTMF tones

--- bluebox.js
+++ bluebox.js
@@ -45,7 +45,7 @@ const schema2 = {1: {'frequencies': [700,900],
 
 const dtmf = {1:[697,1209],  2: [697, 1336], 3:[697,1477],4: [770, 1209], 5:
 [770, 1336], 6: [770, 1477], 7: [852, 1209], 8: [852, 1336], 9: [852, 1477],
-'*': [941, 1209] ,0: [941, 1336], '-': [941, 1477], '+':[350,440]};
+'*': [941, 1209] ,0: [941, 1336], '-': [941, 1477], '+':[350,440],'A':[697,1633], 'B':[770,1633],'C':[852,1633],'D':[941,1633]};
 
 var currentKeys = new Set();
 
@@ -430,13 +430,18 @@ function bindRotaryKeyEvents(){
 function bindToneKeyEvents(){
 
 	$(document).on('keydown', function(e){
-		update(e.key);
-		var thisKey = $('.key').filter(function(){return $(this).html()===e.key}).addClass('active');
+
+		var key = e.key.toUpperCase();
+
+		update(key);
+		var thisKey = $('.key').filter(function(){return $(this).html()===key}).addClass('active');
 	});
 
 	$(document).on('keyup',function(e){
-		stopMF(e.key);
-		$('.key').filter(function(){return $(this).html() === e.key}).removeClass('active');
+		var key = e.key.toUpperCase();
+
+		stopMF(key);
+		var thisKey = $('.key').filter(function(){return $(this).html()===key}).removeClass('active');
 	});
 
 
